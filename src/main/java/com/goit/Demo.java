9e6a73cd803772aded970dd500d43ca9d896e2f2
LubovLisenko <lubovlisenko1985@gmail.com>
2023-04-12 22:29:49 +0200

    homeWork1/Maven

---
+++ src/main/java/com/goit/Demo.java
@@ -0,0 +1,26 @@
+package com.goit;
+
+import com.goit.model.NameLastname;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        try (InputStream stream = Demo.class.getClassLoader().getResourceAsStream("ex1.json")) {
+            Gson gson = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .serializeNulls()
+                    .create();
+            NameLastname nameLastname = gson.fromJson(new String(stream.readAllBytes()), NameLastname.class);
+            System.out.println(nameLastname);
+            System.out.println(("==============================="));
+            String toJson = gson.toJson(nameLastname);
+            System.out.println(toJson);
+
+        }
+
+    }
+}
